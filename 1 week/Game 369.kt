f7e8aaca91a95ad34696e0d847ebbd720b7afd99
imkumsu <139525970+imkumsu@users.noreply.github.com>
2023-08-03 17:18:09 +0900

    Create Game 369.kt

---
+++ 1 week/Game 369.kt
@@ -0,0 +1,77 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        int num = ; // 숫자를 입력해야 합니다.
+        if (num > 0) {
+            System.out.println("positive number");
+        } else if (num < 0) {
+            System.out.println("negative number");
+        } else {
+            System.out.println("zero");
+        }
+    }
+}
+class HelloWorld {
+    public static void main(String[] args) {
+        int age = 26;
+        if (age >= 19) {
+            System.out.println("adult");
+        } else {
+            System.out.println("minor");
+        }
+    }
+}
+//0부터 1씩 증가하는 100개의 숫자를 출력
+//100부터 1씩 감소하는 100개의 숫자를 출력력
+class HelloWorld {
+    public static void main(String[] args) {
+        for(int i = 0;i >100;,i++;)
+                    System.out.println(i)
+        if i>100;
+        break
+    }
+}
+//369게임 1부터 100까지 결과물 출력
+//한글 프로그래밍과 비교해보기
+class HelloWorld {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i < 101) {
+            if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+                System.out.println("clap");
+            } else {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
+
+369게임
+
+1.숫자에 3,6,9 중 하나라도 들어가면 박수를 친다.
+
+2.들어가지 않으면 해당 숫자를 출력한다.
+
+Java에는 기본 자료형과 참조 자료형의 두 가지 유형의 자료형이 있습니다.기본 자료형
+
+- 불리언 자료형 : 참 또는 거짓을 저장할 수 있는 자료형입니다.
+- 숫자 자료형
+- 정수 자료형 : 정수를 저장할 수 있는 자료형입니다.
+- short 자료형 : 2의 보수 16비트 정수를 저장할 수 있는 자료형입니다.
+- int 자료형 : 2의 보수 32비트 정수를 저장할 수 있는 자료형입니다.
+- long 자료형 : 2의 보수 64비트 정수를 저장할 수 있는 자료형입니다.
+- 실수 자료형 : 실수를 저장할 수 있는 자료형입니다.
+- float 자료형 : 32비트 단정밀도 부동 소수점을 저장할 수 있는 자료형입니다.
+- double 자료형 : 64비트 배정밀도 부동 소수점을 저장할 수 있는 자료형입니다.
+- 문자 자료형 : 문자를 저장할 수 있는 자료형입니다.
+- char 자료형 : 유니코드 문자를 저장할 수 있는 자료형입니다.
+
+참조 자료형
+
+- 클래스 자료형 : 객체를 정의하는 자료형입니다.
+- 인터페이스 자료형 : 클래스가 구현해야 하는 메소드의 목록을 정의하는 자료형입니다.
+- 배열 자료형 : 같은 유형의 값의 컬렉션을 저장하는 자료형입니다.
+- 열거형 자료형 : 고유한 값의 집합을 저장하는 자료형입니다.
+- 자료형은 프로그램의 정확성과 효율성을 보장하는 데 중요합니다. 잘못된 자료형을 사용하면 프로그램이 실행되지 않거나 오작동할 수 있습니다.
+
+369 게임은 숫자에 3, 6, 9 중 하나가 있으면 박수를 치고, 없으면 해당 숫자를 출력하는 게임이다. Java에는 기본 자료형과 참조 자료형이 있으며, 자료형은 프로그램의 정확성과 효율성을 보장하는 데 중요하다.
